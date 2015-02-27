840de77354998b16363f082926ccab039c236b67
malnakli <hassoun@outlook.com>
2015-02-26 20:46:20 -0500

    add Jbuilder to render json object: https://github.com/rails/jbuilder

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,21 @@
 class RecipesController < ApplicationController
     def index
+
         @recipes = if params[:keywords]
-                      test = Recipe.where('name like ?', "%#{params[:keywords]}%")
-                      arrayHolder = []
-                      test.each do |item|
-                          arrayHolder.push({
-                                               id: item.id,
-                                               name: item.name,
-                                               instructions: item.instructions
-                                           })
-                      end
-                      render :json => arrayHolder
-                  else
-                      []
-                  end
+                       test = Recipe.where('name like ?', "%#{params[:keywords]}%")
+                       # the following code is replace by index.json.jbuilder file
+                       #               arrayHolder = []
+                       #               test.each do |item|
+                       #                   arrayHolder.push({
+                       #                                        id: item.id,
+                       #                                        name: item.name,
+                       #                                        instructions: item.instructions
+                       #                                    })
+                       #               end
+                       #               render :json => arrayHolder
+                   else
+                       []
+                   end
+
     end
 end
